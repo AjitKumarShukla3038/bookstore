9402c2137269b5cedf98176c1fe5c58452f97322
Ajit <ajit9shukla2001@gmail.com>
2023-10-27 16:49:13 +0530

    update setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -159,7 +159,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ajit9shukla2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'iwnl uasw cdkl thjj'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_PORT = 587
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'  # Corrected setting
